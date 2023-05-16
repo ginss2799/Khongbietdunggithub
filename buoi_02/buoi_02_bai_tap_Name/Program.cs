631409f82e5bccf2e2d431e4780db5464a45ef37
ginss2799 <buikhanhngan27081999@gmail.com>
2023-05-16 14:24:16 +0700

    baitapC

---
+++ buoi_02/buoi_02_bai_tap_Name/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name");
+
+            string yourName = Console.ReadLine ();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
