b618d62de92ca0845764ce3def38f23494b56bdf
ginss2799 <buikhanhngan27081999@gmail.com>
2023-05-18 15:22:27 +0700

    fisrt commit

---
+++ buoi04/buoi04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace buoi04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linear Equation Resolver");
+            Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+
+            Console.WriteLine("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.WriteLine("The solution is: {0}!", solution);
+            }    
+
+            else
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("The solution is all x!");
+                }    
+                else
+                {
+                    Console.Write("Nod solution!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
